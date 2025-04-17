ced30fdb2ad2a3fa1a3aa6fb7a3789253247333b
Lalit Gangwar <notlal1t@gmail.com>
2025-04-17 23:29:23 +0530

    Started React project setup

---
+++ src/App.js
@@ -0,0 +1,40 @@
+// src/App.js
+import React, { useState } from 'react';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTask = () => {
+    if (input.trim() !== "") {
+      setTasks([...tasks, input]);
+      setInput("");
+    }
+  };
+
+  const deleteTask = (index) => {
+    const newTasks = tasks.filter((task, i) => i !== index);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Todo App</h1>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={addTask}>Add</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task} <button onClick={() => deleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
